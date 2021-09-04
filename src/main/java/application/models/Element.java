180cdc139e59cbf52f624eee029ec13e22da989f
Z-Szabadkai <zoltan.szabadkai@gmail.com>
2021-09-04 11:40:09 +0200

    Added dragon and element class

---
+++ src/main/java/application/models/Element.java
@@ -0,0 +1,11 @@
+package application.models;
+
+public class Element {
+
+    private String elementName;
+    private byte[] icon_img;
+
+    public Element(String elementName) {
+        this.elementName = elementName;
+    }
+}
---
+++ src/main/java/application/models/Rarity.java
@@ -0,0 +1,11 @@
+package application.models;
+
+public enum Rarity {
+
+    COMMON,
+    RARE,
+    VERY_RARE,
+    EPIC,
+    LEGENDARY,
+    HEROIC
+}
