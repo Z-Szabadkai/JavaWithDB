1235eb9cebec129d4fa2ad979333461bfeecb59e
Z-Szabadkai <zoltan.szabadkai@gmail.com>
2021-09-04 10:41:43 +0200

    Initial commit

---
+++ src/main/java/application/Main.java
@@ -0,0 +1,12 @@
+package application;
+
+import application.database.DBEngine;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DBEngine engine = new DBEngine();
+
+        System.out.println(engine.isConnected());
+    }
+}
